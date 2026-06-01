4cc946d31230218bd79f52f357a089b19a887f27
mxdoffffff <romankozhukhov1@gmail.com>
2026-06-01 11:24:48 +0300

    use env for DATABASE_URL

--- database.py
+++ database.py
@@ -1,7 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import DeclarativeBase
-DATABASE_URL = "postgresql://postgres:postgres@db:5432/notes_db"
+import os
+from dotenv import load_dotenv
+load_dotenv()
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:postgres@localhost:5432/notes_db")
 
 engine = create_engine(DATABASE_URL)
 
